2d86b029b0020803be30784e0f9e2c1e8bc7e7e9
widaehyeon <85887111+widaehyeon@users.noreply.github.com>
2022-08-03 00:12:53 +0900

    백준 11654 아스키 코드

---
+++ src/bj/bj_11654.java
@@ -0,0 +1,13 @@
+package bj;
+
+import java.util.Scanner;
+
+public class bj_11654 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		String n = sc.next();
+		sc.close();
+		char c = n.charAt(0);
+		System.out.println((int)c);
+	}
+}
